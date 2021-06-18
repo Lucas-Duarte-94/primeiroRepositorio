8f42a076b09079b669a26e7ea6092d422d3ef8d1
Lucas Duarte <lucasduarte_94@hotmail.com>
2021-06-18 01:03:55 -0300

    adicionando a pasta pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import TesteModal from './pages/TesteModal';
+import Camisa from './pages/Camisa';
+
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  background-color: #DDDDDD;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return(
+    <Body>
+      <BrowserRouter>
+        <header>
+          <h1 style={{paddingLeft:10}}>Link to pratice content pages</h1>
+          <nav>
+            <ul>
+            <li>
+                <Link to="/" >Home</Link>
+              </li>
+              <li>
+                <Link to="/About" >Teste de modal</Link>
+              </li>
+              <li>
+                <Link to="/produto/camisa" >Camisa</Link>
+              </li>
+              <li>
+                <Link to="/produto/chinelo" >Chinelo</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <hr />
+
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <p style={{padding: 15}}> Por enquanto nada aqui!</p>
+            </Route>
+            <Route exact path="/About">
+              <TesteModal />
+            </Route>
+            <Route exact path="/produto/:tipo">
+              <Camisa />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Body>
+  )
 }
+
+ 
 
 export default App;
